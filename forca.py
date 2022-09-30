404b7ec1a73db742f6af16d554cd8924741f8325
gabrieldevcarvalho <gabrieldecarvalho@outlook.com>
2022-09-30 16:39:08 -0300

    Avancando em Python

--- forca.py
+++ forca.py
@@ -4,7 +4,24 @@ def jogar():
     print('***Bem vindo ao jogo da Forca***!')
     print('*********************************')
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper(r)):
+                print(f'Encontrei a letra {letra} na posicao {index}.dd')
+            index = index + 1
+
+        print('Jogando...')
+
     print('Fim de Jogo!')
 
-if(__name__ == "__main__"):
+if (__name__ == "__main__"):
     jogar()
