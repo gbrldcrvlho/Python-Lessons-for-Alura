9dad27db8b6a8163fedc9bfc8e419323fe8d3345
gbrldcrvlho <gabrieldecarvalho@outlook.com>
2022-10-10 20:01:22 -0300

    Avancando em Python 02

--- forca.py
+++ forca.py
@@ -1,26 +1,56 @@
+import random
+
 def jogar():
 
     print('*********************************')
     print('***Bem vindo ao jogo da Forca***!')
     print('*********************************')
 
-    palavra_secreta = "banana"
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    # letras_faltando = str(letras_acertadas.count('_'))
+    # print('Aindam faltam acertar {} letras'.format(letras_faltando))
+
     enforcou = False
     acertou = False
+    tentativas = 0
+
+    print(letras_acertadas)
 
     while (not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper(r)):
-                print(f'Encontrei a letra {letra} na posicao {index}.dd')
-            index = index + 1
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            tentativas += 1
 
-        print('Jogando...')
+        enforcou = tentativas == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
 
+    if (acertou):
+        print('Voce ganhou!')
+    else:
+        print('Voce perdeu!')
     print('Fim de Jogo!')
 
 if (__name__ == "__main__"):
