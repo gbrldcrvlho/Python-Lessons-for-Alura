0424b95eb3da9d7180c72a7b9ba5289405c85d49
gbrldcrvlho <gabrieldecarvalho@outlook.com>
2022-10-12 17:11:49 -0300

    Avancando em Python 02

--- forca.py
+++ forca.py
@@ -5,27 +5,18 @@ def jogar():
     mensagem_abertura()
     palavra_secreta = solicitar_palavra_secreta()
     letras_acertadas = solicitar_letras_acertadas(palavra_secreta)
-
-    # letras_faltando = str(letras_acertadas.count('_'))
-    # print('Aindam faltam acertar {} letras'.format(letras_faltando))
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
     tentativas = 0
 
-    print(letras_acertadas)
-
     while (not enforcou and not acertou):
 
-        chute = input("Qual letra? ")
-        chute = chute.strip().upper()
+        chute = solicitar_chute()
 
         if (chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if (chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1
+            seleciona_chute_correto(chute, letras_acertadas, palavra_secreta)
         else:
             tentativas += 1
 
@@ -34,11 +25,9 @@ def jogar():
         print(letras_acertadas)
 
     if (acertou):
-        print('Voce ganhou!')
+        mensagem_sucesso()
     else:
-        print('Voce perdeu!')
-    print('Fim de Jogo!')
-
+        mensagem_fracasso()
 
 
 def mensagem_abertura():
@@ -46,6 +35,11 @@ def mensagem_abertura():
     print('*********************************')
     print('***Bem vindo ao jogo da Forca***!')
     print('*********************************')
+
+def solicitar_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+    return chute
 
 def solicitar_palavra_secreta():
     arquivo = open("palavras.txt", "r")
@@ -63,6 +57,19 @@ def solicitar_palavra_secreta():
 
 def solicitar_letras_acertadas(palavra):
     return ["_" for letra in palavra]
+
+def seleciona_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
+
+def mensagem_sucesso():
+    print('Voce ganhou!')
+
+def mensagem_fracasso():
+    print('Voce perdeu!')
 
 if (__name__ == "__main__"):
     jogar()
